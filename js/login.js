2ac20f8b9dc3820898e90f281bed94c0b1010393
Maxime <maxime.rache@gmail.com>
2023-04-05 11:12:37 +0200

    Ajouts inté formulaire de login

--- js/login.js
+++ js/login.js
@@ -4,8 +4,8 @@ const password = document.getElementById("password");
 
 const envelope = document.createElement("i");
 envelope.classList.add("fa-regular", "fa-envelope");
-email.appendChild(envelope);
+form.appendChild(envelope);
 
 const lock = document.createElement("i");
 lock.classList.add("fa-solid", "fa-lock");
-password.appendChild(lock);
+form.appendChild(lock);
