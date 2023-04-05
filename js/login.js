0c873e8d83297ca5270da01170ff305e034fbb7b
Maxime <maxime.rache@gmail.com>
2023-04-05 16:42:39 +0200

    Formulaire de connexion intégré

--- js/login.js
+++ js/login.js
@@ -1,11 +1,50 @@
-const form = document.getElementById("form-login");
-const email = document.getElementById("label-email");
-const password = document.getElementById("label-password");
+const form = document.getElementById("login-form");
+const email = document.getElementById("email");
+const labelEmail = document.getElementById("label-email");
+const password = document.getElementById("password");
+const labelPassword = document.getElementById("label-password");
+const submit = document.getElementById("login-submit");
 
 const envelope = document.createElement("i");
 envelope.classList.add("fa-regular", "fa-envelope");
-email.appendChild(envelope);
+labelEmail.appendChild(envelope);
 
 const lock = document.createElement("i");
 lock.classList.add("fa-solid", "fa-lock");
-password.appendChild(lock);
+labelPassword.appendChild(lock);
+
+email.addEventListener("input", function() {
+
+
+    if (isValidEmail(email.value)) {
+
+        envelope.style.color = "greenyellow";
+    }  else {
+
+        envelope.style.color = "white";
+    }
+
+});
+
+password.addEventListener("input", function() {
+
+    if (password.value.length >= 8) {
+
+        lock.style.color = "greenyellow";
+    } else {
+
+        lock.style.color = "white";
+    }
+});
+
+
+function isValidEmail(email) {
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegex.test(email);
+}
+
+submit.addEventListener("click", function(event) {
+
+    event.preventDefault();
+    /* Appel à la fonction de connexion */
+});
