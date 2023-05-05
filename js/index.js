208b565e56881b619751d9ecef4baae860160fa8
Maxime <maxime.rache@gmail.com>
2023-05-05 12:12:58 +0200

    Fix polices, ajout Open Sans, fix intégration et amélioration du script

--- js/index.js
+++ js/index.js
@@ -6,22 +6,11 @@ async function getBanner() {
     const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=NTVDouKU0ldR4FlffcNFsG1Pd1uLWd1JTEoF7V7b")
     const result = await response.json()
 
-    const banner = document.createElement("figure")
-    const img = document.createElement("img")
-    const figcaption = document.createElement("figcaption")
-    const explanation = document.createElement("p")
-
-    banner.setAttribute("id", "banner")
-    
-    explanation.setAttribute("id", "explanation")
-    img.src = result.hdurl
-    figcaption.innerText = result.copyright
-    explanation.innerText = result.explanation
-
-    banner.append(img, figcaption)
-    container.append(banner, explanation)
-    container.insertBefore(banner, header)
-    container.insertBefore(explanation, header)
+    document.getElementById("banner-img").src = result.hdurl
+    document.getElementById("banner-title").innerText = result.title
+    document.getElementById("explanation").innerText = result.explanation
+    document.getElementById("banner").style.display = "block"
+    //console.log(result)
 }
 
 getBanner();
