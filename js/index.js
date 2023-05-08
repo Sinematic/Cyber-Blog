a6cba2a314da469a899ea480fa2e02bad8afe28b
Maxime <maxime.rache@gmail.com>
2023-05-08 18:01:53 +0200

    préfix header

--- js/index.js
+++ js/index.js
@@ -8,17 +8,14 @@ async function getBanner() {
     const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=NTVDouKU0ldR4FlffcNFsG1Pd1uLWd1JTEoF7V7b")
     const result = await response.json()
 
-    document.getElementById("banner-img").src = result.hdurl
-    document.getElementById("banner-title").innerText = result.title
-    explanation.innerText = result.explanation
-    document.getElementById("banner").style.display = "block"
-    //console.log(result)
+    if(response.ok) {
+        document.getElementById("banner-img").src = result.hdurl
+        document.getElementById("banner-title").innerText = result.title
+        explanation.innerText = result.explanation
+        document.getElementById("banner").style.display = "block"
+    } else document.getElementById("banner").style.display = "none"
 }
 
 getBanner();
 
-
-info.addEventListener("click", () => {
-    console.log("toto")
-    explanation.classList.toggle("slide")
-})
+info.addEventListener("click", () => explanation.classList.toggle("slide"))
